9903580d53095388781afbdf7781fe65043ce256
exp1rex <maxx0n.walk@gmail.com>
2022-10-04 22:19:14 +0800

    Добавленны меню навигации и стили к нему

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,11 @@ function Header() {
         <header>
             <div>
                 <span className="logo">Game Store</span>
+                <ul className='nav'>
+                    <li>Про нас</li>
+                    <li>Контакты</li>
+                    <li>Кабинет</li>
+                </ul>
             </div>
             <div className="presentation"></div>
         </header>
