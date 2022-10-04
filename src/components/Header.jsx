85406d3e5245869fc8648c69114763e1f78421a7
exp1rex <maxx0n.walk@gmail.com>
2022-10-04 21:42:25 +0800

    Добавлены Header и Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <span className="logo">Game Store</span>
+            </div>
+            <div className="presentation"></div>
+        </header>
+    )
+}
+
+export default Header
